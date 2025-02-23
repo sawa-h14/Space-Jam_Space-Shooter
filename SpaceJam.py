09fdd9dfd053174eb33f7a9389488055c69c0a10
sawa-h14 <sawa.hirosawa.sawa14@gmail.com>
2025-02-23 18:51:17 -0500

    feat: add key events to move the spaceship

--- SpaceJam.py
+++ SpaceJam.py
@@ -4,12 +4,19 @@ from panda3d.core import *
 from direct.showbase.Loader import *
 import DefensePaths as defensePaths
 import SpaceJamClasses
+from direct.task import Task
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        
+        # Mouse control.
+        base.disableMouse()
+        
         self.SetupScene()
-
+        self.SetCamera()
+        self.SetKeyBindings()
+        
         fullCycle = 60
         for j in range(fullCycle):
             SpaceJamClasses.Drone.droneCount += 1
@@ -64,6 +71,128 @@ class MyApp(ShowBase):
         position = unitVec * radius * 250 + centralObject.modelNode.getPos()
         color = 0, 0, 255, 1 # blue
         SpaceJamClasses.Drone(self.loader, "./Assets/Drone Defender/DroneDefender.obj", self.render, droneName, "./Assets/Drone Defender/octotoad1_auv.png", position, 5, color)
+    
+    def SetKeyBindings(self):
+        # All of our key bindings for our spaceship's movement.
+        self.accept('w', self.Thrust, [1])
+        self.accept('w-up', self.Thrust, [0])
+        self.accept('s', self.Drag, [1])
+        self.accept('s-up', self.Drag, [0])
+        self.accept('a', self.LeftTurn, [1])
+        self.accept('a-up', self.LeftTurn, [0])
+        self.accept('d', self.RightTurn, [1])
+        self.accept('d-up', self.RightTurn, [0])
+        self.accept('arrow_up', self.UpTurn, [1])
+        self.accept('arrow_up-up', self.UpTurn, [0])
+        self.accept('arrow_down', self.DownTurn, [1])
+        self.accept('arrow_down-up', self.DownTurn, [0])
+        self.accept('q', self.LeftTilt, [1])
+        self.accept('q-up', self.LeftTilt, [0])
+        self.accept('e', self.RightTilt, [1])
+        self.accept('e-up', self.RightTilt, [0])
+    
+    def Thrust(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyThrust, 'forward-thrust')
+        else:
+            self.taskMgr.remove('forward-thrust')
+
+    def ApplyThrust(self, task):
+        rate = 5
+        trajectory = self.render.getRelativeVector(self.Hero.modelNode, Vec3.forward())
+        trajectory.normalize()
+        self.Hero.modelNode.setFluidPos(self.Hero.modelNode.getPos() + trajectory * rate )
+        return Task.cont
+
+    def Drag(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyDrag, 'drag')
+        else:
+            self.taskMgr.remove('drag')
+
+    def ApplyDrag(self, task):
+        rate = 5
+        trajectory = self.render.getRelativeVector(self.Hero.modelNode, Vec3.forward())
+        trajectory.normalize()
+        self.Hero.modelNode.setFluidPos(self.Hero.modelNode.getPos() - trajectory * rate )
+        return Task.cont
+
+    def LeftTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyLeftTurn, 'left-turn')
+        else:
+            self.taskMgr.remove('left-turn')
+
+    def ApplyLeftTurn(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setH(self.Hero.modelNode.getH() + rate)
+        return Task.cont
+    
+    def RightTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyRightTurn, 'right-turn')
+        else:
+            self.taskMgr.remove('right-turn')
+
+    def ApplyRightTurn(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setH(self.Hero.modelNode.getH() - rate)
+        return Task.cont
+
+    def UpTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyUpTurn, 'up-turn')
+        else:
+            self.taskMgr.remove('up-turn')
+
+    def ApplyUpTurn(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setP(self.Hero.modelNode.getP() + rate)
+        return Task.cont
+
+    def DownTurn(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyDownTurn, 'down-turn')
+        else:
+            self.taskMgr.remove('down-turn')
+
+    def ApplyDownTurn(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setP(self.Hero.modelNode.getP() - rate)
+        return Task.cont
+
+    def LeftTilt(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyLeftTilt, 'left-tilt')
+        else:
+            self.taskMgr.remove('left-tilt')
+
+    def ApplyLeftTilt(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setR(self.Hero.modelNode.getR() + rate)
+        return Task.cont
+
+    def RightTilt(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.ApplyRightTilt, 'right-tilt')
+        else:
+            self.taskMgr.remove('right-tilt')
+
+    def ApplyRightTilt(self, task):
+        # Half a degree every frame.
+        rate = .5
+        self.Hero.modelNode.setR(self.Hero.modelNode.getR() - rate)
+        return Task.cont
+    
+    def SetCamera(self):
+        self.disableMouse()
+        self.camera.reparentTo(self.Hero.modelNode)
+        self.camera.setFluidPos(0, 1, 0)
 
 app = MyApp()
 app.run()
